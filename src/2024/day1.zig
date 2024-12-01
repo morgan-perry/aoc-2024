4185fe010012356667f5ff26a51a5d8e4a6f77c8
Morgan Perry <morganperry@posteo.net>
2024-12-01 21:44:02 +1000

    Part 1 of Day 1 2024

--- src/2024/day1.zig
+++ src/2024/day1.zig
@@ -1,20 +1,45 @@
 const std = @import("std");
 const mem = std.mem;
+const assert = std.debug.assert;
 
 input: []const u8,
 allocator: mem.Allocator,
-
-fn getDistance(x: u32, y: u32) u32 {
-    return @abs(x - y);
-}
 
 pub fn part1(this: *const @This()) !?i64 {
     // Initial Thoughts:
     // Duplicates matter
     // Distance is abs(x - y)
     // Sort both lists and iterate
-    _ = this;
-    return null;
+
+    // Input only has 1000 lines
+    var first_list = std.mem.zeroes([1000]i32);
+    var second_list = std.mem.zeroes([1000]i32);
+
+    var it = mem.tokenizeSequence(u8, this.input, "\n");
+
+    var idx: u16 = 0;
+    while (it.next()) |slice| {
+        var it2 = mem.tokenizeSequence(u8, slice, " "); // Why does tokenize only like one space here?
+        first_list[idx] = try std.fmt.parseInt(i32, it2.next().?, 10);
+        second_list[idx] = try std.fmt.parseInt(i32, it2.next().?, 10);
+
+        idx += 1;
+        assert(idx <= 1000);
+    }
+
+    mem.sort(i32, &first_list, {}, comptime std.sort.asc(i32));
+    mem.sort(i32, &second_list, {}, comptime std.sort.asc(i32));
+    // Quick insanity test
+    assert(first_list[0] <= first_list[first_list.len - 1]);
+    assert(second_list[0] <= second_list[first_list.len - 1]);
+
+    var total_distance: u32 = 0;
+
+    for (first_list, second_list) |first, second| {
+        total_distance += @abs(first - second);
+    }
+
+    return total_distance;
 }
 
 pub fn part2(this: *const @This()) !?i64 {
@@ -33,10 +58,4 @@ test "it should do nothing" {
 
     try std.testing.expectEqual(null, try problem.part1());
     try std.testing.expectEqual(null, try problem.part2());
-}
-
-test "getDistanceCheck" {
-    const x1: u32 = 1;
-    const y1: u32 = 2;
-    try std.testing.expectEqual(getDistance(x1, y1), 1);
 }
